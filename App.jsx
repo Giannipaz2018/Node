cadfc44fc07f0a12ccc47341810de46ba6fb16a9
Giannipaz2018 <giannipaz2018@gmail.com>
2024-09-18 09:20:24 +0200

    update App.jsx

--- App.jsx
+++ App.jsx
@@ -13,3 +13,16 @@ ReactDOM.render(
     root: ''
   }} />,
   mountNode);
+  
+ const elemento = document.getElementById('container');
+/*const boton1 = React.createElement('button', {"data-id":123}, 'tócame');
+const boton2 = React.createElement('button', {"data-id":456}, 'tócame');
+
+const group = React.createElement(React.Fragment, null, boton1, boton2);*/
+
+const group= (<React.Fragment>
+  <button id='123'>toca</button>
+  <button id='456'>toca</button>
+</React.Fragment>)
+
+ReactDOM.render(group, elemento);
